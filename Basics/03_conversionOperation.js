5cabfe9aa5423e645e04d29139ba6374f09ff604
Ravirajsinh Gohil <ravi.empowergrowth@gmail.com>
2025-11-19 10:55:22 +0000

    ConversionOperation

--- Basics/03_conversionOperation.js
+++ Basics/03_conversionOperation.js
@@ -3,27 +3,34 @@ let score = "33"
 console.log(typeof score);
 console.log(score);
 
-// conversion in string to number
+/* conversion in string to number */
 let valueInNumber = Number(score)
+
 console.log(typeof valueInNumber);
+
 console.log(valueInNumber);
-// "33"    = 33
+// "33" = 33
 // "33abc" = NaN
 // true = 1 / false = 0
 
-// conversion in boolean
+/* conversion in boolean */
 let isLoggedIn = 0
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
+
 console.log(typeof booleanIsLoggedIn);
+
 console.log(booleanIsLoggedIn);
-// 1      = true / 0 = flase
-// ""     = false
+// 1 = true / 0 = flase
+// "" = false
 // "GOKU" = true
 
-// conversion number to string
+/* conversion number to string */
 let IsNum = 33
 
 let ValueInString = String(IsNum)
+
 console.log(typeof ValueInString);
+
 console.log(ValueInString);
+// 33 = "33"
