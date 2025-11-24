3dc040bce49747b9740c4c021e9172b8610df60f
Ravirajsinh Gohil <ravi.empowergrowth@gmail.com>
2025-11-24 15:44:51 +0000

    DateAndTime

---
+++ Basics/07_DatesIn.js
@@ -0,0 +1,10 @@
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleTimeString());
+console.log(typeof myDate);
+
+let myCreatedDate = new Date(2025,0)
