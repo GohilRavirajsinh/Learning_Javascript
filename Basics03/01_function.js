6c21c043b9355cc4076b1a646ed17a5c2ceb044f
Ravirajsinh Gohil <ravi.empowergrowth@gmail.com>
2025-12-01 08:15:24 +0000

    BasicFunction

--- Basics03/01_function.js
+++ Basics03/01_function.js
@@ -41,11 +41,36 @@ const result = addTwoNumbers(3, 5)
 
 // 3...............
 function loginUserMessage(username = "Default") { // This username is define for by default, for example if any value define by default than use this!
-    if(!username) { // Execute when Default value not called 
+    if (!username) { // Execute when Default value not called 
         console.log("Please enter the username");
         return
     }
     return `${username} Just logged in`
 }
-console.log(loginUserMessage())  // Call Default Direct
+// console.log(loginUserMessage())  // Call Default Direct
 // console.log(loginUserMessage("Goku"))
+
+function calculateCartPrice(val1, val2, ...num1) {  // ...num(Rest Operator) combine all values in array
+    return num1
+}
+// console.log(calculateCartPrice(200, 300, 400, 2000))
+
+const User = {
+    username: "GokuGaming",
+    price: 199
+}
+function handleUserObject(anyobject) {
+    // console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+// handleUserObject(User) // object value call in function
+
+handleUserObject({ // Default
+    username: "DirectNew",
+    price: 999
+})
+
+const myNewArray = [200, 400, 500, 1000]
+function ArrayHandleFunction(getArray){
+    return getArray[1]
+}
+console.log(ArrayHandleFunction(myNewArray))
