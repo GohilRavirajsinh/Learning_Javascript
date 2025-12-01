c19e38aabda32c6bcf35627b78faa7f0008995e9
Ravirajsinh Gohil <ravi.empowergrowth@gmail.com>
2025-12-01 09:31:25 +0000

    BasicScopes

--- Basics03/02_scopes.js
+++ Basics03/02_scopes.js
@@ -1,4 +1,4 @@
-// {} scope declaration define with function,if-else etc... / Object are also but that for object declaration 
+// {} scope declaration define with function,if-else,loops etc... / Object are also but that for object declaration 
 
 // the globel scope
 var c = 300
@@ -15,3 +15,31 @@ if (true) {
 console.log("globel scope: ", a);
 // console.log(b);
 // console.log(c);
+
+
+function one() {  // Nested Scope {}
+    const nested = "Nested Scopes Print!"
+
+    function two() {
+        const game = "Battlegrounds Mobile India"
+        console.log(nested)
+    }
+    // console.log(game);
+
+    two()
+    two()
+}
+// one()
+
+
+if (true) {   // Nested Scope {}
+    const game = "Battlegrounds Mobile India"
+    if (game === "Battlegrounds Mobile India") {
+        const nestifelse = "Nested If Else"
+        console.log(`The Game name is ${game} & this is ${nestifelse} example!`);
+    }
+    // console.log(nestifelse); //False
+    // console.log(game);  // True
+}
+// console.log(nestifelse);  // False
+// console.log(game);  // False
