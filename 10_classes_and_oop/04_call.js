e94f14388bcc9bd22e8555dd0142a1c61a55084d
Ravirajsinh Gohil <ravi.empowergrowth@gmail.com>
2026-02-09 13:47:07 +0000

    '.call' to access one paranthesis to another function using 'this.' keyword

--- 10_classes_and_oop/04_call.js
+++ 10_classes_and_oop/04_call.js
@@ -4,7 +4,7 @@ function setUsername(username) {
 
 // constructor function to create a user object
 function createUser(username, email, password) {
-    setUsername.call(this.username) // // call use karne se setusername() function idhar call hota hai,else vo vahi se run hoke stack se bahar chala jayega! or 'this' use karne se vo username idhar call hoga - generally this use karne se vo sabhi paranthases dusre function ko de deta hai
+    setUsername.call(this.username) // call use karne se setusername() function idhar call hota hai,else vo vahi se run hoke stack se bahar chala jayega! or 'this' use karne se vo username idhar call hoga - generally this use karne se vo sabhi paranthases dusre function ko de deta hai
 
     this.email = email
     this.password = password
