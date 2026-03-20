f6dd5cea16622b0ef00a5ef4c579e6d60eae840a
Ravirajsinh Gohil <ravi.empowergrowth@gmail.com>
2026-03-20 10:05:29 +0000

    Explain Target and source

--- 02_Basics/04_Object.js
+++ 02_Basics/04_Object.js
@@ -32,6 +32,9 @@ const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "c", 4: "d"}
 const obj3 = {5: "e", 6: "f"}
 const CombineArray = {...obj1, ...obj2, ...obj3}
+// const CombineArray = {target, source}
+// const CombineArray = Object.assign({}, obj1, obj2, obj3)
+// const CombineArray = {target, source} if we dont create empty {} than the target is automatically obj1 and other objects is source automatically but if we create empty{} first and than add obj1,2,3 than the target is {} and other objects are source!
 // console.log(CombineArray);
 
 
