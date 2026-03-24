2d8c4664a915df6d8e0ca978b48d739cf448159c
Ravirajsinh Gohil <ravi.empowergrowth@gmail.com>
2026-03-24 07:22:47 +0000

    optimised code

--- 07_Projects/02_BMI_Calculator/script.js
+++ 07_Projects/02_BMI_Calculator/script.js
@@ -1,28 +1,29 @@
 const form = document.querySelector('form');
 // this usecase will give you empty value
 // const height = parseInt(document.querySelector('#height').value); // height here not define cause, here defined height is load with form so stor e empty value inside that time so it cannot run when click on submit!
-form.addEventListener('submit', function (e) {
-  e.preventDefault(); // it is stop form to send browser
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
   const height = parseInt(document.querySelector('#height').value);
   const weight = parseInt(document.querySelector('#weight').value);
-  const results = document.querySelector('#results');
+  const result = document.querySelector('#results');
+  console.log(height);
+  console.log(weight);
+  console.log(result);
 
   if (height === '' || height < 0 || isNaN(height)) {
-    results.innerHTML = `Please give a valid height ${height}`;
+    result.innerHTML = 'give a valid Height!';
   } else if (weight === '' || weight < 0 || isNaN(weight)) {
-    results.innerHTML = `Please give a valid weight ${weight}`;
+    result.innerHTML = 'give a valid Weight!';
   } else {
-    const bmi = ((height * weight) / 1000).toFixed(2);
-    // show result
-    results.innerHTML = `<span>${bmi}</span>`; // print ans
-    // check weight range
-    const bmi_weight = document.querySelector('#bmi-weight');
-    if (bmi <= 18.6) {
-      bmi_weight.innerHTML = `Under Weight`;
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    // show the result
+    if (bmi < 18.6) {
+      result.innerHTML = `<span>Under Weight Ans:${bmi}</span>`;
     } else if (bmi >= 18.6 && bmi <= 24.9) {
-      bmi_weight.innerHTML = `Normal Range`;
+      result.innerHTML = `<span>Normal Range Ans:${bmi}</span>`;
     } else if (bmi >= 24.9) {
-      bmi_weight.innerHTML = `Overweight`;
+      result.innerHTML = `<span>Overweight Ans:${bmi}</span>`;
     }
   }
 });
