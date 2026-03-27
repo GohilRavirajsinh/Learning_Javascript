6de4d75f4f8ef58b8a4957866093091f9ac65245
Ravirajsinh Gohil <ravi.empowergrowth@gmail.com>
2026-03-27 05:48:48 +0000

    optimise code

--- 10_classes_and_oop/10_getter_setter.js
+++ 10_classes_and_oop/10_getter_setter.js
@@ -5,27 +5,22 @@ class User {
     }
 
     get email(){
-        return this._email.toUpperCase()
+        return `${this._email} new member`
     }
 
-    set email(value){
-        this._email = value
+    set email(getterValue){
+        this._email = getterValue
     }
 
     get password(){
-        // 'password' is the getter property, called like: obj.password
-        // '_password' is a class property (by "convention", the underscore means "private" or "for internal use")
-        // Typical usage: the setter assigns to this._password and the getter returns a processed value (or the raw value).
-        // Example:
-        return `${this._password}AddSomePoints` // it merge password
-        // return `${this._password}jinhoo`
-        // return this._password.toUpperCase()
+        return `${this._password} uniq Pass`
     }
 
-    set password(pass){
-        this._password = pass
+    set password(getterValue){
+        this._password = getterValue
     }
 }
 
-const goku = new User("goku@dbz.com", 23343)
+const goku = new User("goku@gmail.com", "235")
+console.log(goku.email)
 console.log(goku.password)
