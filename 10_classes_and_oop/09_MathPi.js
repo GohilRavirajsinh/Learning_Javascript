0505ba174e33993621d6741b8219e13f9bda9d2a
Ravirajsinh Gohil <ravi.empowergrowth@gmail.com>
2026-03-27 05:23:01 +0000

    update code

--- 10_classes_and_oop/09_MathPi.js
+++ 10_classes_and_oop/09_MathPi.js
@@ -10,35 +10,42 @@ console.log(descroptor) //value: 3.141592653589793,
 // aisi object property hai? jise khudse bhi create kar sakte hai? Yesssss for ex
 // const myNewObj = Object.create(null) like that
 
-const chai = {
-    name: "ginger chai",
-    price: 250,
-    isAvailable: true,
 
-    orderChai(){
-        console.log("Chai nhin bani!")
+const Maps = {
+    name: "Erangle",
+    size: "8x8",
+    isEventAvailable: true,
+
+    mapExpired: function(){
+        console.log(`Map has been Expired`)
     }
 }
-console.log(chai)
-// now check iski bhi descripor properties hai? or hai to use set kar sakte hai?
-console.log(Object.getOwnPropertyDescriptor(chai, "name"))
-/*Ans: value: 'ginger chai',
-       writable: true,
-       enumerable: true,
-       configurable: true */
-// we can change this property
+console.log(Object.getOwnPropertyDescriptor(Maps, "name")) // by default 
 
-Object.defineProperty(chai, "name",{ // it change specific for name property
+// we can change here properties for our code
+
+Object.defineProperty(Maps, "name", {
     // writable: false,
-    enumerable: false // ye false hai to name vali value niche for loop me nhin jayegi, to aise configuration kar sakte hai ki jitni bhi properties hai vo apne loop ka part hi na bne, to aise #price or sab pe bhi lga sakte jiski bhi need ho.
+    enumerable: false,
+    configurable: true
 })
-// Now check
-console.log(Object.getOwnPropertyDescriptor(chai, "name")) // See result
 
-// iske uper loop lgakar dekh lete hai? lets see
-for (let [key, value] of Object.entries(chai)) {
+Object.defineProperty(Maps, "size", {
+    // writable: false,
+    enumerable: true,
+    configurable: true
+})
+
+Object.defineProperty(Maps, "isEventAvailable", {
+    // writable: false,
+    enumerable: false,
+    configurable: true
+})
+console.log(Object.getOwnPropertyDescriptor(Maps, "name")) // after change
+
+for (let [key, value]  of Object.entries(Maps)) {
     if (typeof value !== 'function') {
-        
+
         console.log(`${key} : ${value}`)
     }
 }
